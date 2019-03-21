f56c78500e626dfe0f70b942dde35d6b2cb44c6d
Me <no.spam@example.com>
2019-03-21 23:56:43 +0100

    Exclude the build directory

--- bmp.h
+++ bmp.h
@@ -2,14 +2,10 @@
 #ifndef _BMP_H_  // prevent recursive inclusion
 #define _BMP_H_
 
-
 #include "BitmapInformation.h"
 
-
-BMPImage* read_bmp(FILE* fp, char** error);
-bool      check_bmp_header(BMPHeader* bmp_header, FILE* fp);
 bool      write_bmp(FILE* fp, BMPImage* image, char** error);
 void      free_bmp(BMPImage* image);
-BMPImage* crop_bmp(BMPImage* image, int x, int y, int w, int h, char** error);
+BMPImage * CreateBitmapFromScan0(int32_t w, int32_t h, uint8_t* scan0)
 
 #endif  // bmp.h
